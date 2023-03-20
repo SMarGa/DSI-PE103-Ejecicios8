66bee9790d9ff3bfdadc50f92e5b27a50803e67a
Saul Martin <alu0101405810@ull.edu.es>
2023-03-20 10:04:45 +0000

    Test de Ci

--- tests/Strategy.spec.ts
+++ tests/Strategy.spec.ts
@@ -11,4 +11,12 @@ describe("Test de Solver", () => {
 
     expect(mySolver.logic()).to.be.eql(expectedArray);
   });
+
+  it("BubbleSort", () => {
+    const myArray = [1, 4, 2, 3];
+    const expectedArray = [1, 2, 3, 4];
+    const mySolver = new Solver(myArray, new BubbleSort());
+
+    expect(mySolver.logic()).to.be.eql(expectedArray);
+  });
 });
